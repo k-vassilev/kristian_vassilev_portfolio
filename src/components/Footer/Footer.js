67e1f14d2ce6e049ce0857d07861992d2a924b74
Kristian Vassilev <kvasilev@devrix.com>
2021-12-27 14:33:25 +0200

    Added footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,31 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+		<LinkList>
+			<LinkColumn>
+				<LinkTitle>Call</LinkTitle>
+				<LinkItem href="tel:+359-886-901-202">+359-886-901-202</LinkItem>
+			</LinkColumn>
+			<LinkColumn>
+				<LinkTitle>Email</LinkTitle>
+				<LinkItem href="mailto:kristian.g.vassilev@gmail.com">kristian.g.vassilev@gmail.com</LinkItem>
+			</LinkColumn>
+		</LinkList>
+		<SocialIconsContainer>
+			<CompanyContainer>
+				<Slogan>Live as if you were to die tomorrow. Learn as if you were to live forever.</Slogan>
+			</CompanyContainer>
+			<SocialIconsContainer>
+				<SocialIcons href="https://github.com/k-vassilev">
+					<AiFillGithub size="3rem" />
+				</SocialIcons>
+				<SocialIcons href="https://www.linkedin.com/in/kristian-vassilev-86076227/">
+					<AiFillLinkedin size="3rem" />
+				</SocialIcons>
+			</SocialIconsContainer>
+		</SocialIconsContainer>
+	</FooterWrapper>
   );
 };
 

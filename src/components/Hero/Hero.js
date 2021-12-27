84d3a6e8453623ce99824df7a6043eb8ebb72b2a
Kristian Vassilev <kvasilev@devrix.com>
2021-12-27 12:41:12 +0200

    Added hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+	  <LeftSection>
+		  <SectionTitle main center>
+			  Welcome To <br />
+			  Kristian Vassilev`s Portfolio
+		  </SectionTitle>
+		  <SectionText>
+		  	A tech-savvy professional, who focusses on Web developmenet, ERP Systems and Business consulting.
+		  </SectionText>
+		  <Button onClick={() => window.location = "https://www.etk-design.com/portfolio/"}>Learn More</Button>
+	  </LeftSection>
+  </Section>
 );
 
 export default Hero;

adcf16d9c0f64e7c7c3097e06ea7190b06ca61f5
Kristian Vassilev <kvasilev@devrix.com>
2021-12-29 12:54:08 +0200

    Changed button styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { LeftSection, ExternalLinks } from './HeroStyles';
 
 const Hero = () => (
   <Section row nopadding>
@@ -14,7 +14,7 @@ const Hero = () => (
 		  <SectionText>
 		  	A tech-savvy professional, with focus on Web and App development with experience in ERP implementations and business consulting. I am also the CEO of a small web agency - <a href="https://www.etk-design.com/about/" target="_blank" rel="noreferrer">Ephedra-TK</a>.
 		  </SectionText>
-		  <a href="../../../Kristian_Vassilev_CV_28_12_2021.pdf" target="_blank" rel="noreferrer"><Button>View my Resume</Button></a>
+		  <ExternalLinks href="../../../Kristian_Vassilev_CV_28_12_2021.pdf" target="_blank" rel="noreferrer">View my Resume</ExternalLinks>
 	  </LeftSection>
   </Section>
 );
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -17,3 +17,35 @@ export const LeftSection = styled.div`
     margin: 0 auto;
   }
 `;
+
+export const ExternalLinks = styled.a`
+color: #FFF;
+background: none;
+border: 1px solid rgba(255, 255, 255, 0.33);
+box-sizing: border-box;
+border-radius: 999px;
+padding: 16px 24px;
+font-weight: 600;
+font-size: 18px;
+line-height: 16px;
+width: fit-content;
+margin-top: 32px;
+margin-bottom: 80px;
+cursor: pointer;
+transition: 0.4s ease;
+&:focus {
+  outline: none;
+}
+
+&:hover {
+  color: #0f1624;
+  background: #fff;
+  border: 1px solid #fff;
+}
+
+&:active {
+  background: #e0e4eb;
+  border: 1px solid #304169;
+  box-shadow: inset 0px 2px 1px rgba(46, 49, 55, 0.15), inset 0px 0px 4px rgba(20, 20, 55, 0.3);
+}
+`;
